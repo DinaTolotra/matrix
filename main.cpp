28b6739d5324a3983b12846665e743767fc8d6a5
DinaTolotra <RatsinjomananaTolotra@gmail.com>
2024-05-27 17:39:45 +0300

    Usage

--- main.cpp
+++ main.cpp
@@ -6,14 +6,44 @@
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-    Matrice a(4), b(4);
-    a.fill(2); b.fill(0);
+    Matrice a(3), b(3);
+    char oper;
 
-    for (int i = 0; i < b.getWidth(); ++i) {
-        b.at(i, i) = 2;
+    cout << "Input a:" << endl;
+    for (int i=0; i<a.getHeight(); i++) {
+        for (int j=0; j<a.getWidth(); j++) {
+            int input;
+            cin >> input;
+            a.at(j, i) = input;
+        }
     }
 
-    (a * b).print();
+    cout << "Input b:" << endl;
+    for (int i=0; i<a.getHeight(); i++) {
+        for (int j=0; j<a.getWidth(); j++) {
+            int input;
+            cin >> input;
+            b.at(j, i) = input;
+        }
+    }
+
+    cout << "Operateur: " << endl;
+    cin >> oper;
+
+    switch (oper) {
+    case '+':
+        cout << "Addition: " << endl;
+        (a+b).print();
+        break;
+    case '-':
+        cout << "Soustraction: " << endl;
+        (a-b).print();
+        break;
+    case '*':
+        cout << "Multiplication: " << endl;
+        (a*b).print();
+        break;
+    }
 
     return 0;
 }
